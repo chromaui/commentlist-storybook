5ac38b691594d79e39532a5e5fee6eb9f26a3efe
Tom Coleman <tom@thesnail.org>
2017-04-21 10:36:43 +1000

    Added styles for the `HasData` use case

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,6 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function({loading, comments, totalCount}) {
+const CommentListDiv = styled.div`
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: #333;
+  display: inline-block;
+  vertical-align: top;
+  width: 265px;
+`;
+
+const CommentItemDiv = styled.div`
+  font-size: 12px;
+  line-height: 14px;
+
+  clear: both;
+  height: 48px;
+  margin-bottom: 10px;
+
+  box-shadow: rgba(0,0,0,.2) 0 0 10px 0;
+  background: linear-gradient(120deg, rgba(248, 248, 254, .95), rgba(250, 250, 250, .95));
+  border-radius: 48px;
+`;
+
+const AvatarDiv = styled.div`
+  float: left;
+  position: relative;
+  overflow: hidden;
+  height: 48px;
+  width: 48px;
+  margin-right: 14px;
+
+  background: #dfecf2;
+  border-radius: 48px;
+`;
+
+const AvatarImg = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  background: #999;
+`;
+
+const MessageDiv = styled.div`
+  overflow: hidden;
+  padding-top: 10px;
+  padding-right: 20px;
+`;
+
+const AuthorSpan = styled.span`
+font-weight: bold;
+`;
+const TextSpan = styled.span``;
+
+export default function CommentList({ loading, comments, totalCount }) {
   if (loading) {
     return <div>empty</div>;
   }
@@ -9,5 +64,17 @@ export default function({loading, comments, totalCount}) {
     return <div>loading</div>;
   }
 
-  return <div>{comments.length} of {totalCount}</div>;
+  return (
+    <CommentListDiv>
+      {comments.map(({ text, author: { name, avatar } }) => (
+        <CommentItemDiv>
+          <AvatarDiv><AvatarImg src={avatar} /></AvatarDiv>
+          <MessageDiv>
+            <AuthorSpan>{name}</AuthorSpan>{' '}
+            <TextSpan>{text}</TextSpan>
+          </MessageDiv>
+        </CommentItemDiv>
+      ))}
+    </CommentListDiv>
+  );
 }
--- src/stories/CommentList.js
+++ src/stories/CommentList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {storiesOf} from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 
 import CommentList from '../CommentList';
 
@@ -7,7 +7,9 @@ storiesOf('CommentList', CommentList)
   .add('HasData', () => (
     <CommentList comments={testComments.slice(0, 3)} totalCount={3} />
   ))
-  .add('Paginated', () => <CommentList comments={testComments} totalCount={10} />)
+  .add('Paginated', () => (
+    <CommentList comments={testComments} totalCount={10} />
+  ))
   .add('Loading', () => <CommentList loading={true} />)
   .add('Empty', () => <CommentList comments={[]} />);
 
@@ -16,35 +18,35 @@ const testComments = [
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     author: {
       name: 'Luke',
-      avatar: 'luke.jpg',
-    },
+      avatar: 'luke.jpg'
+    }
   },
   {
-    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
     author: {
       name: 'Leah',
-      avatar: 'leah.jpg',
-    },
+      avatar: 'leah.jpg'
+    }
   },
   {
     text: 'Duis aute irure dolor in reprehenderit in voluptate.',
     author: {
       name: 'Han',
-      avatar: 'han.jpg',
-    },
+      avatar: 'han.jpg'
+    }
   },
   {
-    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
     author: {
       name: 'Poe',
-      avatar: 'poe.jpg',
-    },
+      avatar: 'poe.jpg'
+    }
   },
   {
     text: 'Duis aute irure dolor in reprehenderit in voluptate.',
     author: {
       name: 'Finn',
-      avatar: 'finn.jpg',
-    },
-  },
+      avatar: 'finn.jpg'
+    }
+  }
 ];
